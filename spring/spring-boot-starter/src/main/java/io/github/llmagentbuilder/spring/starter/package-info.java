a89823d99ff39444104fd48c9501458d8f67594e
Fu Cheng <alexcheng1982@gmail.com>
2024-10-04 16:42:53 +1300

    no more spring

--- spring/spring-boot-starter/src/main/java/io/github/llmagentbuilder/spring/starter/package-info.java
+++
@@ -1,1 +0,0 @@
-package io.github.llmagentbuilder.spring.starter;
--- spring/spring-dev/src/main/kotlin/io/github/llmagentbuilder/spring/dev/AgentDevConfiguration.kt
+++
@@ -1,9 +0,0 @@
-package io.github.llmagentbuilder.spring.dev
-
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@ComponentScan
-class AgentDevConfiguration {
-}
